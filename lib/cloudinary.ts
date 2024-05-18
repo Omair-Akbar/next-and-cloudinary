64c595cc567ca87362005891c840e2999cd129c8
Laptop Wala <muhammadomairakbar@gmail.com>
2024-05-18 15:30:04 +0500

    Initial commit from Create Next App

---
+++ lib/cloudinary.ts
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dloxfvl80', 
+  api_key: '345898944698193', 
+  api_secret: 'FYTSkD14DQrUeHEHxNfbU1qZbcY' 
+});
+
+export default cloudinary;

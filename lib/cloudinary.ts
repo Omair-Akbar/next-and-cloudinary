0bcbbb169afdf62d4a1392b7dee305ddfeb6b686
Omair Akbar <159688251+Omair-Akbar@users.noreply.github.com>
2024-05-20 01:28:41 +0500

    Update

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary';
           
 cloudinary.config({ 
-  cloud_name: 'dloxfvl80', 
-  api_key: '345898944698193', 
-  api_secret: 'FYTSkD14DQrUeHEHxNfbU1qZbcY' 
+  cloud_name: 'xxxxxxxxxxxxx', 
+  api_key: 'xxxxxxxxxxxxxxxx', 
+  api_secret: 'xxxxxxxxxxxxxx' 
 });
 
 export default cloudinary;
